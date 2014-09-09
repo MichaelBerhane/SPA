4ccd186bb70dd85c738f9980cc9e6eacbfd1722e
Michael Berhane <michael@michaelberhane.org>
2014-09-09 17:25:06 +0100

    added controller and questions

--- js/controller.js
+++ js/controller.js
@@ -3,5 +3,17 @@ var appControllers = angular.module('appControllers', []);
 appControllers.controller('QuestionController', ['$scope', '$http', function($scope, $http){
 	$http.get('js/data.json').success(function(data){
 		$scope.question = data;
+		$scope.count = Number(0);
+
+		/* Increase the counter */
+
+		$scope.button = function(){
+			if($scope.count < 5){
+				$scope.count++;
+			}
+			
+		}
+
 	});
 }]);
+
