7ad26557d4fe40fcaff3b3fecd08e75083c55f79
Michael Berhane <michael@michaelberhane.org>
2014-09-10 12:45:05 +0100

    Added answer function

--- js/controller.js
+++ js/controller.js
@@ -4,16 +4,40 @@ appControllers.controller('QuestionController', ['$scope', '$http', function($sc
 	$http.get('js/data.json').success(function(data){
 		$scope.question = data;
 		$scope.count = Number(0);
+		$scope.answer_count = 0;
 
 		/* Increase the counter */
 
-		$scope.button = function(){
+		$scope.button = function(num){
 			if($scope.count < 5){
 				$scope.count++;
-			}
+			}	
 			
+			var score = Number(num);
+			answer(score);
+		}
+
+		/* getter */
+
+		$scope.getcounter = function(){
+			return $scope.count;
+		}
+
+		var answer = function(answer){
+			console.log("current answer: " + answer);
+			$scope.answer_count += answer;
+			console.log("current score: " + $scope.answer_count);
 		}
 
 	});
+}]);
+
+
+appControllers.controller('AnswerController', ['$scope', '$http', function($scope, $http){
+
+
+
+
+
 }]);
 
