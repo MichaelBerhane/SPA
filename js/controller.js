1b1f4b5a80e9041fa4c58adf54f8f61ad6a11cdc
Michael Berhane <michael@michaelberhane.org>
2014-09-10 21:23:48 +0100

    Correct result to result.html

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,12 @@
 var appControllers = angular.module('appControllers', []);
 
-	/* A factory to share data across Controllers */
+	/*********************************************
+	 * A factory to share data across Controllers
+	***********************************************/
 
 	angular.module('myApp').factory('Data', function(){
 
-		var score = "hello";
+		var score = 0;
 
 		return {
 			return_score: function(){
@@ -16,22 +18,31 @@ var appControllers = angular.module('appControllers', []);
 		};
 	});
 
-	/* Controller for the questions */
+	/*******************************
+	* Controller for the questions
+	***********************************/
 
 	appControllers.controller('QuestionController', ['$scope', '$http', 'Data', function($scope, $http, Data){
 		$http.get('js/data.json').success(function(data){
 
 
-			$scope.question = data;
-			$scope.count = 0;
-			$scope.answer_count = 0;
+			$scope.question = data; // Json data //
+			$scope.count = 0;  			// Rotate through Json Array
+			$scope.answer_count = 0; // The score //
+			var button_click = 0; // How many times clicked
 
 			/* Increase the counter */
 
 			$scope.button = function(num){
-				if($scope.count < 5){
-					$scope.count++;
-					answer(num);
+				button_click++;  // button increase every time an answer is clicked
+
+				if($scope.count <= 5 && button_click <= 5){
+					$scope.count++;  // go to the next question
+					answer(num);    // add to answer
+				}
+
+				if(button_click === 6){
+					answer(num);   // add answer on the final question.
 				}
 
 			}
@@ -47,14 +58,15 @@ var appControllers = angular.module('appControllers', []);
 				console.log("current answer: " + answer);
 				console.log("current score: " + $scope.answer_count);
 				Data.final_score($scope.answer_count);
-				console.log(Data.return_score());
 			}
 
 		});
 	}]);
 
 
-	/* A Controller for the results */
+	/*******************************
+	* A Controller for the results 
+	*******************************/
 
 	appControllers.controller('ResultController', ['$scope', '$http', 'Data', function($scope, $http, Data){
 			$scope.answer = Data.return_score();
